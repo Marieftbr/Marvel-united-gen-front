44facfbd831e3a32e6903290a6e56f8b363ebec8
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-05 17:18:56 +0200

    page backoffice

---
+++ src/pages/Backoffice.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BackOfficeForm from "../components/BackOffice_form";
+import BackOfficeHeader from "../components/BackOffice_header";
+
+export default function BackOffice() {
+  return (
+    <div>
+      <BackOfficeHeader />
+      <BackOfficeForm />
+    </div>
+  );
+}
