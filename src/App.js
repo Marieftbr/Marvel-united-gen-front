6613bfb53cf088b89ff3dfb33e8dab86251fa5e8
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-05 17:20:22 +0200

    declaration of routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BackOffice from "./pages/Backoffice";
 
 function App() {
-  return <div className="App">Toto</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/backoffice" element={<BackOffice />} />
+        </Routes>
+      </Router>
+      <h1>Toto</h1>
+    </div>
+  );
 }
 
 export default App;
