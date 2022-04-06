1fad56710eebe7fc1b669af3d1e9d2fd304dc950
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-06 15:27:12 +0200

    change content of this page

--- src/components/BackOffice_content.js
+++ src/components/BackOffice_content.js
@@ -4,21 +4,13 @@ export default function BackOfficeContent() {
   return (
     <div>
       <div className="back-office__btn-wrappers">
-        <h1>Name of the choice</h1>
-        <div className="back-office__list">
-          <table>
-            <th>
-              <td>#</td>
-              <td>Picture</td>
-              <td>Name</td>
-            </th>
-            <tbody>
-              <td>1</td>
-              <td>Toto</td>
-              <td>toto</td>
-            </tbody>
-          </table>
-        </div>
+        <h1>Welcome</h1>
+        <p>
+          Here is the backoffice, you can add Hero/Vilains/Both Characters, you
+          can add Locations and add Boxes you have. For generate a party of
+          Marvel United, you have to save the data of what boxes, characters and
+          locations you have. Please enter your data.
+        </p>
       </div>
     </div>
   );
