ee18626b885e689b692be3ac7ef8f60f70fbdf15
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-06 15:27:47 +0200

    add correct value href for the links (correct warnings)

--- src/components/BackOffice_header.js
+++ src/components/BackOffice_header.js
@@ -4,12 +4,12 @@ export default function BackOfficeHeader() {
   return (
     <div className="back-office__header">
       <div className="back-office__logo-wrapper">
-        <a href="#">Logo</a>
+        <a href="/">Logo</a>
       </div>
       <div className="back-office__link-wrapper">
-        <a href="#">Characters</a>
-        <a href="#">Boxes</a>
-        <a href="#">Locations</a>
+        <a href="/characters">Characters</a>
+        <a href="/boxes">Boxes</a>
+        <a href="/locations">Locations</a>
       </div>
     </div>
   );
