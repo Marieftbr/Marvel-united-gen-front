1b0dc858f16e73b9719d73385eb55f847d982461
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-06 15:28:06 +0200

    character pages creation

---
+++ src/pages/Backoffice_characters.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BackOfficeCharacterCard from "../components/BackOffice_character-card";
+import BackOfficeHeader from "../components/BackOffice_header";
+
+export default function BoCharacters() {
+  return (
+    <div>
+      <BackOfficeHeader />
+      <BackOfficeCharacterCard />
+    </div>
+  );
+}
