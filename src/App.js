14e455f2a6a94b60e6bffc3054bdc9a20c674da0
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-06 15:26:55 +0200

    add fontawesome package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BackOffice from "./pages/Backoffice";
+import BackOfficeCharacters from "./pages/Backoffice_characters";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBoxOpen, faMask, faUser } from "@fortawesome/free-solid-svg-icons";
+library.add(faBoxOpen, faMask, faUser);
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/backoffice" element={<BackOffice />} />
+          <Route
+            path="/backoffice/characters"
+            element={<BackOfficeCharacters />}
+          />
         </Routes>
       </Router>
       <h1>Toto</h1>
