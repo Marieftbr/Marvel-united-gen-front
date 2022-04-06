978d167f63f09534caf820a487a03a81f911575d
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-06 15:46:02 +0200

    add request to bdd

--- src/pages/Backoffice_characters.js
+++ src/pages/Backoffice_characters.js
@@ -1,12 +1,27 @@
 import React from "react";
 import BackOfficeCharacterCard from "../components/BackOffice_character-card";
 import BackOfficeHeader from "../components/BackOffice_header";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function BoCharacters() {
+  const [data, setData] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/characters");
+      setData(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <BackOfficeHeader />
-      <BackOfficeCharacterCard />
+      <BackOfficeCharacterCard data={data} />
     </div>
   );
 }
