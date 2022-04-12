90c9d9afec754e4802abf84c69639ba869f3ddfd
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-12 17:25:33 +0200

    you can add a location now

--- src/components/BackOffice_location-form.js
+++ src/components/BackOffice_location-form.js
@@ -1,24 +1,102 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import BackOfficeHeader from "./BackOffice_header";
 
 export default function LocationForm() {
-  return (
+  const [boxes, setBoxes] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchBoxes = async () => {
+    const response = await axios.get("http://localhost:3000/box");
+    setBoxes(response.data);
+    setIsLoading(false);
+  };
+
+  const pictureRef = React.createRef();
+
+  const [name, setName] = useState("");
+  const [box, setBox] = useState("");
+  const [picture, setPicture] = useState("");
+
+  const sendData = async () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("box_id", box);
+    formData.append("picture", picture);
+
+    const response = await axios.post(
+      "http://localhost:3000/locations",
+      formData
+    );
+    console.log(response.data);
+  };
+
+  const handleBox = (event) => {
+    setBox(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await sendData();
+  };
+
+  useEffect(() => {
+    fetchBoxes();
+  }, []);
+
+  return isLoading ? (
+    <div>En cours de chargement...</div>
+  ) : (
     <div>
       <BackOfficeHeader />
       <h1 className="back-office__location-form-title">Add a Location</h1>
-      <form id="locationForm" className="back-office__location-form-container">
+      <form
+        id="locationForm"
+        className="back-office__location-form-container"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="Name"
           className="back-office__location-form-input"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
         />
         <div className="back-office__location-form-select-box-container">
           <label htmlFor="">Box</label>
-          <select name="" id="">
-            <option value="">toto</option>
+          <select name="boxes" onChange={handleBox}>
+            {boxes.map((box) => {
+              return (
+                <option value={box._id} key={box._id}>
+                  {box.name}
+                </option>
+              );
+            })}
           </select>
         </div>
-        <input type="file" className="back-office__location-form-input" />
+        <label
+          htmlFor=""
+          onClick={(event) => {
+            pictureRef.current.dispatchEvent(
+              new MouseEvent("click", {
+                bubbles: false,
+                cancelable: true,
+                view: window,
+              })
+            );
+          }}
+          value={picture}
+        ></label>
+        <input
+          type="file"
+          className="back-office__location-form-input"
+          ref={pictureRef}
+          onChange={(event) => {
+            setPicture(event.target.files[0]);
+          }}
+        />
 
         <button className="back-office__location-form-btn">Save</button>
       </form>
