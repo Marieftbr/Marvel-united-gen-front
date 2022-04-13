61fc714523935207e2fbda339a162ba378f56918
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-13 12:56:15 +0200

    add functional delete btn and add no functional update btn for the moment

--- src/components/BackOffice_boxes-card.js
+++ src/components/BackOffice_boxes-card.js
@@ -1,7 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import axios from "axios";
 
 export default function BackOfficeBoxesCard(props) {
+  const deleteData = async (id) => {
+    await axios.delete("http://localhost:3000/box", { data: { id: id } });
+    window.location.reload();
+  };
+
   return (
     <div>
       {props.data.map((box) => {
@@ -10,6 +16,17 @@ export default function BackOfficeBoxesCard(props) {
             <FontAwesomeIcon
               icon="fa-solid fa-box"
               className="back-office__boxes-card-icon"
+            />
+            <FontAwesomeIcon
+              icon="fa-solid fa-pen"
+              className="back-office__card-pen-icon"
+            />
+            <FontAwesomeIcon
+              icon="fa-solid fa-trash"
+              className="back-office__card-trash-icon"
+              onClick={() => {
+                deleteData(box._id);
+              }}
             />
             <img
               src={box.picture.url}
