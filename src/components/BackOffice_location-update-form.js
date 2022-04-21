034f89bad4bad2086e36e2f23049962493cb978d
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-21 18:16:16 +0200

    add form for update location

---
+++ src/components/BackOffice_location-update-form.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import BackOfficeHeader from "./BackOffice_header";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default function LocationUpdateForm() {
+  const pictureRef = React.createRef();
+
+  const [picture, setPicture] = useState("");
+  const [name, setName] = useState("");
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [dataAlreadySetted, setDataAlreadySetted] = useState(null);
+  const { id } = useParams();
+
+  const fetchThisData = async () => {
+    const response = await axios.get(`http://localhost:3000/location/${id}`, {
+      params: {
+        id: id,
+      },
+    });
+    console.log(response.data);
+    setDataAlreadySetted(response.data);
+
+    setName(response.data.name);
+    setPicture(response.data.picture.url);
+
+    setIsLoading(false);
+  };
+
+  const updateData = async () => {
+    const formData = new FormData();
+    formData.append("picture", picture);
+    formData.append("name", name);
+
+    await axios.put(`http://localhost:3000/location/${id}`, formData);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await updateData();
+  };
+  console.log(picture);
+
+  useEffect(() => {
+    fetchThisData();
+  }, []);
+
+  return isLoading && dataAlreadySetted ? (
+    <div>En cours de chargement...</div>
+  ) : (
+    <div>
+      <BackOfficeHeader />
+      <h1 className="back-office__character-form-title">
+        Update {dataAlreadySetted ? dataAlreadySetted.name : "a character"}
+      </h1>
+      <form
+        id="characterForm"
+        className="back-office__character-form-container"
+        onSubmit={handleSubmit}
+      >
+        <img
+          src={dataAlreadySetted ? dataAlreadySetted.picture.url : picture}
+          alt=""
+          onClick={(event) => {
+            pictureRef.current.dispatchEvent(
+              new MouseEvent("click", {
+                bubbles: false,
+                cancelable: true,
+                view: window,
+              })
+            );
+          }}
+        />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          className="back-office__character-form-input"
+          ref={pictureRef}
+          onChange={(event) => {
+            setPicture(event.target.files[0]);
+          }}
+        />
+        <input
+          type="text"
+          className="back-office__character-form-input"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+
+        <button className="back-office__character-form-btn">Save</button>
+      </form>
+    </div>
+  );
+}
