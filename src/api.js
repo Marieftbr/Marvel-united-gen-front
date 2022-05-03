18f72a0398f28f2122ec38fac512e0c02a3d9286
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-05-03 18:17:04 +0200

    remove slash

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const url = "https://marvel-united-generator.herokuapp.com/";
+const url = "https://marvel-united-generator.herokuapp.com";
 
 export function apiGet(path, params) {
   const token = Cookies.get("token");
