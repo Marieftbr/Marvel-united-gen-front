08e09c419afc4b63fbdb1be212edc5db067be428
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-05-03 13:29:14 +0200

    functionality of login form

--- src/components/BackOffice_form.js
+++ src/components/BackOffice_form.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Navigate, useNavigate } from "react-router-dom";
+import { apiPost } from "../api";
 
-export default function BackOfficeForm() {
+export default function BackOfficeForm(props) {
+  const navigate = useNavigate();
+
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const response = await apiPost("/login", {
+      password,
+    });
+    props.saveToken(response.data);
+    navigate("/backoffice");
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    login();
+  };
+
   return (
     <div className="back-office__form-container">
       <h1 className="back-office__h1">BackOffice</h1>
-      <form className="back-office__form">
-        <input
-          className="back-office__input"
-          type="email"
-          placeholder="Email"
-        />
+      <form className="back-office__form" onSubmit={submitForm}>
         <input
           className="back-office__input"
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
         <button className="back-office__btn">Connexion</button>
       </form>
