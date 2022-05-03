b387047796e450e88bfc689660769f3ae4cadd04
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-05-03 13:28:45 +0200

    add token and cookie system

--- src/pages/Backoffice.js
+++ src/pages/Backoffice.js
@@ -1,14 +1,28 @@
 import React from "react";
-// import BackOfficeForm from "../components/BackOffice_form";
+import BackOfficeForm from "../components/BackOffice_form";
 import BackOfficeHeader from "../components/BackOffice_header";
 import BackOfficeContent from "../components/BackOffice_content";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
 export default function BackOffice() {
+  const [token, setToken] = useState(Cookies.get("token") || null);
+
+  const saveToken = (value) => {
+    setToken(value);
+    Cookies.set("token", value);
+  };
+  console.log(token);
+
   return (
     <div>
       <BackOfficeHeader />
-      {/* <BackOfficeForm /> */}
-      <BackOfficeContent />
+
+      {token ? (
+        <BackOfficeContent />
+      ) : (
+        <BackOfficeForm token={token} saveToken={saveToken} />
+      )}
     </div>
   );
 }
