bab447d7930531148256464549b3f528d63761b2
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-05-04 15:53:04 +0200

    repare home link from homeheader

--- src/components/Home_Header.js
+++ src/components/Home_Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function HomeHeader() {
   return (
     <div className="home__header-wrapper">
-      <Link to="/home">Home</Link>
+      <Link to="/">Home</Link>
       <Link to="/backoffice" className="home__header-links">
         Back Office
       </Link>
